5a62a3d3abc6e88ef5cdf673d70cde500b68557f
Alexey <alexdat2000@gmail.com>
2025-04-06 16:59:28 +0300

    docker for middlware

--- middleware/main.go
+++ middleware/main.go
@@ -4,16 +4,15 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 func main() {
-	port := flag.Int("port", 8080, "Port to listen on")
+	listen := flag.String("port", "8080", "Port to listen on")
 	flag.Parse()
 
-	log.Printf("Listening on %d", *port)
+	log.Printf("Listening on %s", *listen)
 	http.HandleFunc("/calculate", handleCalculate)
-	err := http.ListenAndServe(":"+strconv.Itoa(*port), nil)
+	err := http.ListenAndServe(*listen, nil)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}

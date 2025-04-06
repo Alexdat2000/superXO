dadcfb831fd0e7db09d46fe7ee9eb57354e05fad
Alexey <alexdat2000@gmail.com>
2025-04-06 18:14:02 +0300

    CORS

--- middleware/go.mod
+++ middleware/go.mod
@@ -1,3 +1,8 @@
 module github.com/Alexdat2000/superXO/middleware
 
 go 1.24.1
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/rs/cors v1.11.1
+)
--- middleware/main.go
+++ middleware/main.go
@@ -4,15 +4,25 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
 	listen := flag.String("port", "8080", "Port to listen on")
 	flag.Parse()
 
+	router := mux.NewRouter()
+	handler := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET"},
+	}).Handler(router)
+
+	router.HandleFunc("/calculate", handleCalculate)
+
 	log.Printf("Listening on %s", *listen)
-	http.HandleFunc("/calculate", handleCalculate)
-	err := http.ListenAndServe(*listen, nil)
+	err := http.ListenAndServe(*listen, handler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
